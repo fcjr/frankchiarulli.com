6e237f3324acba03b2d003523133885733baae46
Frank Chiarulli Jr <frank@frankchiarulli.com>
2021-04-03 11:14:23 -0400

    fix prismjs imports

--- src/lib/highlight.ts
+++ src/lib/highlight.ts
@@ -1,13 +1,13 @@
 import Prism from 'prismjs';
 
 // import additional languages here
-import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-go.js';
 
 // import plugins
 // make sure to also include the plugins css in global.css
-import 'prismjs/plugins/line-numbers/prism-line-numbers';
-import 'prismjs/plugins/toolbar/prism-toolbar';
-import 'prismjs/plugins/show-language/prism-show-language';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
+import 'prismjs/plugins/toolbar/prism-toolbar.js';
+import 'prismjs/plugins/show-language/prism-show-language.js';
 
 // disable automatic highlighting
 Prism.manual = true;
