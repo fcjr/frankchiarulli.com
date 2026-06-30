6ae0efa0262600f9bd1fd5a7d12694c1c1311a03
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2026-06-30 05:45:43 -0400

    remove google fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,16 +1,8 @@
 import localFont from "next/font/local";
-import { Merriweather } from "next/font/google";
 
 export const sans = localFont({
   src: "./fonts/CommitMono-400-Regular.woff2",
   display: "swap",
   weight: "400",
   style: "normal",
-});
-
-export const serif = Merriweather({
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["400", "700"],
-  style: ["normal", "italic"],
 });
